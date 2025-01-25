5e78617e22dc76d798e9e49cfc3e43e9c1fff0ee
ReedM99 <rmcalpin@emich.edu>
2025-01-25 11:41:56 -0500

    gets working

--- scripts/client-call.js
+++ scripts/client-call.js
@@ -17,7 +17,7 @@ async function getData() {
         
         const json = await response.json();
         console.log(json);
-        var query = document.querySelector("h3");
+        var query = document.querySelector("#results1");
         if (query) {
             query.textContent = JSON.stringify(json, null, 2);
         } else {
@@ -39,3 +39,88 @@ async function getData() {
         }
     }
 }
+
+async function getDataAll() {
+    var deptget = document.querySelector("#deptAll").value;
+    const url = "http://localhost:3000/departments/All/";
+    var urlanddept = url + deptget;
+    
+    try {
+        const response = await fetch(urlanddept, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+  
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        
+        const json = await response.json();
+        console.log(json);
+        var query = document.querySelector("#results2");
+        if (query) {
+            query.textContent = JSON.stringify(json, null, 2);
+        } else {
+            console.error("Element not found");
+        }
+    } catch (error) {
+        if (error.message.includes('Network response was not ok')) {
+        
+        switch (error.message.split(': ')[1]) {
+            case '0':
+                console.error('Request failed. Possible network issue.');
+                break;
+            case '404':
+                console.error('Resource not found.');
+                break;
+        }
+        } else {
+            console.error('Error:', error);
+        }
+    }
+}
+
+async function getDataDepartmentInstructor() {
+    var deptget = document.querySelector("#deptInst").value;
+    const url = "http://localhost:3000/departments/department_instructor/";
+    var urlanddept = url + deptget;
+    
+    try {
+        const response = await fetch(urlanddept, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+  
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        
+        const json = await response.json();
+        console.log(json);
+        var query = document.querySelector("#results3");
+        if (query) {
+            query.textContent = JSON.stringify(json, null, 2);
+        } else {
+            console.error("Element not found");
+        }
+    } catch (error) {
+        if (error.message.includes('Network response was not ok')) {
+        
+        switch (error.message.split(': ')[1]) {
+            case '0':
+                console.error('Request failed. Possible network issue.');
+                break;
+            case '404':
+                console.error('Resource not found.');
+                break;
+        }
+        } else {
+            console.error('Error:', error);
+        }
+    }
+}
+
