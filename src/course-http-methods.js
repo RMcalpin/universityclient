df2e5032a825b73a2cf6717c2764cac239167a7a
ReedM99 <rmcalpin@emich.edu>
2025-02-17 20:39:42 -0500

    added course html and js, adjusted file names

---
+++ src/course-http-methods.js
@@ -0,0 +1,129 @@
+async function getData() {
+    var courseget = document.querySelector("#dept").value;
+    const url = "http://localhost:3000/courses/";
+    var urlandcourse = url + courseget;
+    
+    try {
+        const response = await fetch(urlandcourse, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+  
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        
+        const json = await response.json();
+        console.log(json);
+        var query = document.querySelector("#results1");
+        if (query) {
+            query.textContent = JSON.stringify(json, null, 2);
+        } else {
+            console.error("Element not found");
+        }
+    } catch (error) {
+        if (error.message.includes('Network response was not ok')) {
+        
+        switch (error.message.split(': ')[1]) {
+            case '0':
+                console.error('Request failed. Possible network issue.');
+                break;
+            case '404':
+                console.error('Resource not found.');
+                break;
+        }
+        } else {
+            console.error('Error:', error);
+        }
+    }
+}
+
+async function getDataAll() {
+    var courseget = document.querySelector("#courseAll").value;
+    const url = "http://localhost:3000/courses/All/";
+    var urlandcourse = url + courseget;
+    
+    try {
+        const response = await fetch(urlandcourse, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+  
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        
+        const json = await response.json();
+        console.log(json);
+        var query = document.querySelector("#results2");
+        if (query) {
+            query.textContent = JSON.stringify(json, null, 2);
+        } else {
+            console.error("Element not found");
+        }
+    } catch (error) {
+        if (error.message.includes('Network response was not ok')) {
+        
+        switch (error.message.split(': ')[1]) {
+            case '0':
+                console.error('Request failed. Possible network issue.');
+                break;
+            case '404':
+                console.error('Resource not found.');
+                break;
+        }
+        } else {
+            console.error('Error:', error);
+        }
+    }
+}
+
+async function getDataCourseSection() {
+    var courseget = document.querySelector("#courseSec").value;
+    const url = "http://localhost:3000/departments/department_instructor/";
+    var urlandcourse = url + courseget;
+    
+    try {
+        const response = await fetch(urlandcourse, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+  
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        
+        const json = await response.json();
+        console.log(json);
+        var query = document.querySelector("#results3");
+        if (query) {
+            query.textContent = JSON.stringify(json, null, 2);
+        } else {
+            console.error("Element not found");
+        }
+    } catch (error) {
+        if (error.message.includes('Network response was not ok')) {
+        
+        switch (error.message.split(': ')[1]) {
+            case '0':
+                console.error('Request failed. Possible network issue.');
+                break;
+            case '404':
+                console.error('Resource not found.');
+                break;
+        }
+        } else {
+            console.error('Error:', error);
+        }
+    }
+}
+
+window.getData = getData;
+window.getDataAll = getDataAll;
+window.getDataCourseSection = getDataCourseSection;
